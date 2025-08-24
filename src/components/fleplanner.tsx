954cb7979a4ccf6a6310827b2908602047d12ff1
Harsh Kahate <hkahate@gmail.com>
2025-08-24 17:22:22 +0530

    fixing an error

--- src/components/fleplanner.tsx
+++ src/components/fleplanner.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { jsPDF } from "jspdf";
 import { Document, Packer, Paragraph, TextRun } from "docx";
 
